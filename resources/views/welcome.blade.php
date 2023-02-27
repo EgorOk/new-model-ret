3aa1548428458f9c4d35d9c787ceb5608dff9d44
EgorOk <egorcud33@gmal.com>
2023-02-27 16:43:02 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+
+@section('title')
+    New-model-ret
+@endsection
+
+@section('main_content')
+    <div class="container">
+        <ul>
+            <li>
+                <a href="/brends">Бренды</a>
+            </li>
+            <li>
+                <a href="/shops">Магазины</a>
+            </li>
+            <li>
+                <a href="/models">Модели</a>
+            </li>
+        </ul>
+    </div>
+@endsection
